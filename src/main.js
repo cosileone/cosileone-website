c0097e81dfc67911768a2ecf8b25aebac7bdce97
Cosi Leone <cosi.leone@icloud.com>
2023-12-22 11:49:03 +0100

    maybe try pushing css to head

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
-import "~/styles/global.css";
+import globalStyles from "~/styles/global.css";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -22,5 +22,9 @@ export default function(Vue, { router, head, isClient }) {
     rel: "stylesheet",
     href:
       "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Overpass:wght@800&display=swap"
+  });
+  head.link.push({
+    rel: "stylesheet",
+    href: globalStyles
   });
 }
