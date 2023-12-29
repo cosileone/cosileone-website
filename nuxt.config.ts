a991d3f441151f9eb706b3e1f875162210495928
Cosi Leone <cosi.leone@icloud.com>
2023-12-29 18:59:11 +0000

    add meta tags to website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Cosi Leone',
+      meta: [
+        {
+          name: 'description',
+          content: 'Cosi Leone is a freelance web developer based in London, New York City, and Florence Italy (Firenze). He provides the best-in-class web development services for e-commerce websites, web and mobile applications, content creators, influencers, and startups of any size.'
+        },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+    }
+  },
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@vueuse/nuxt', ['@nuxtjs/google-fonts', {
     families: {
